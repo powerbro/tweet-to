d330b63f62db2a70eb0dc47c1a4b2b3bc57ded0d
shitij.c <shitij.c@directi.com>
2015-11-18 20:50:01 +0530

    Image assets added

---
+++ Tweet-to/User+CoreDataProperties.m
@@ -0,0 +1,29 @@
+//
+//  User+CoreDataProperties.m
+//  Tweet-to
+//
+//  Created by shitij.c on 17/11/15.
+//  Copyright © 2015 Riva. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "User+CoreDataProperties.h"
+
+@implementation User (CoreDataProperties)
+
+@dynamic banner_image_url;
+@dynamic created_at;
+@dynamic image_url;
+@dynamic name;
+@dynamic profile_image;
+@dynamic profile_image_thumbnail;
+@dynamic user_id;
+@dynamic username;
+@dynamic creates;
+@dynamic followed_by;
+@dynamic following;
+@dynamic timeline;
+
+@end
